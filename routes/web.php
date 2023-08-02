c3ec1ed85a77ca6a017c73b6170477792b58df49
yeremia purba <yeremiapurba14@gmail.com>
2023-08-02 11:59:52 +0700

    integrasi checkout part 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,18 +19,12 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+Route::get('checkout/success', [CheckoutController::class,'success'])->name('checkout.success');
+Route::get('checkout/{camp:slug}', [CheckoutController::class,'create'])->name('checkout.create');
+
 Route::get('sign-in-google',[UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('checkout', function () {
-    return view('checkout');
-})->name('checkout');
-
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
-
-//socialite routes
 
 Route::get('/dashboard', function () {
     return view('dashboard');
